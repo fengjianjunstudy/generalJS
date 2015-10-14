eb49e3c749a71bd97d4cd5fbbbd6f8e48569511e
fjj <fjj_321@126.com>
2015-10-14 13:57:26 +0800

    共享多个参数  函数转换公共函数

--- curry.js
+++ curry.js
@@ -1,3 +1,25 @@
-function curryFn(fn){
+/**
+	应用场景：调用同一个函数，传递的参数绝大多数相同 ，那么这个函数就很适合做为参数传递给通用的函数转换函数（curryFn） 
+*/
+
+
+function curryFn(){
 	var slice=Array.prototype.slice; 
+	var args=slice.call(arguments);
+	var fn=args.shift();
+	return function(){ 
+		var arg=args.concat(slice.call(arguments));
+		return fn.apply(null,arg);
+	}
 }
+
+function add(a,b,c,d){
+	return a+b+c+d;
+}
+
+var add1=curryFn(add,1);
+var sum=add1(2,3,6);
+var sum1=add1(10,20,30)
+console.log("sum"+sum)
+
+console.log("sum1 "+sum1)
