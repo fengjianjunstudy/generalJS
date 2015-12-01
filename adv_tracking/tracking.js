df76cd167538b1c3254055dd781fed522f898a5e
fjj <fjj_321@126.com>
2015-12-01 17:18:44 +0800

    广告位 曝光 点击统计

---
+++ adv_tracking/tracking.js
@@ -0,0 +1,15 @@
+/*
+	@function adv tracking
+	@editer fjj
+	@create_time 20151201
+**/
+(function(widnow,$,undefined){
+	if(window.CSDN.Tracking!=undefined){
+		var CSDN.Tracking={};
+	}
+	
+	CSDN.Tracking.advTrack=function(ele,opt){
+
+	}
+	return window.CSDN.Tracking=CSDN.Tracking;
+})(window,jQuery)
